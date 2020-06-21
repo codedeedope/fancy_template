62dbb2c86c18b10424c8b690a2392f8479f558dd
codedeedope <26957536+codedeedope@users.noreply.github.com>
2020-06-21 08:30:54 +0200

    Template Metaprogramm, das einer array Stelle ihren Index zuweist, tut

--- fancy_template.cpp
+++ fancy_template.cpp
@@ -8,8 +8,41 @@
 #define EX(NUM) int i##NUM = p##NUM();
 #define PTREX(NUM) PTR(NUM) EX(NUM)
 
+#include <array>
+
+template<size_t i, size_t n>
+struct Numbers2 {
+	static void number(std::array<size_t, n>& arr) {
+		arr[i - 1] = i-1;
+		Numbers2<i - 1, n>::number(arr);
+	}
+};
+
+template<size_t n>
+struct Numbers2<0, n> {
+	static void number(std::array<size_t, n>& arr) {
+		return;
+	}
+};
+
+template<size_t n>
+struct Numbers {
+	static std::array<size_t, n> number() {
+		static_assert(n >= 1);
+		std::array<size_t, n> arr;
+		arr[n - 1] = n-1;
+		Numbers2<n-1, n>::number(arr);
+		return arr;
+	}
+};
+
+static const std::array<size_t, 3> theArr(Numbers<3>::number());
+
+
 int main()
 {
+	size_t a = theArr[2];
+
 	std::cout << "Hello World!\n";
 
 	size_t pt = Timer::timerInternalISRList.size();
